b6649d963163e53c228a65867c5e67d09078099e
joycedeluroza <deluroza.joyce.bsis@gmail.com>
2026-03-02 21:16:26 +0800

    Add files via upload

---
+++ DelurozaJoyce_MultiplicationActivity/main.py
@@ -0,0 +1,14 @@
+# Multiply two numbers entered by the user
+
+print()
+print("------------------------------------")
+print("          MULTIPLICATION 101          ")
+print("------------------------------------")
+
+Delurozanum1 = int(input("   Enter the first number: "))
+Delurozanum2 = int(input("   Enter the second number: "))
+print("------------------------------------")
+
+product = Delurozanum1 * Delurozanum2
+print(f"   The product of {Delurozanum1} and {Delurozanum2} is: {product}")
+print("------------------------------------")
